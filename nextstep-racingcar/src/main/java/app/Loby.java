96da786bfaa6561d7eeab2d8adbc9eb11cedba90
seonghoo1217 <seonghoo1217@naver.com>
2023-05-20 16:52:43 +0900

    feat : 게임 시작 출력문

---
+++ nextstep-racingcar/src/main/java/app/Loby.java
@@ -0,0 +1,10 @@
+package app;
+
+import domain.BasicOption;
+
+public class Loby {
+	private static BasicOption basicOption;
+	public static void main(String[] args) {
+		basicOption.startingRacingGame();
+	}
+}
